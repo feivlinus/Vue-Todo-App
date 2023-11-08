3ee354c5dce95641a9e73fccb994586dd0c76127
Feivlinus <para.feivl.dox@gmail.com>
2023-11-08 01:30:33 +0100

    Changed the style with help of the grid system of vuetify and applied some colors. Change h1 to a toolbar.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
-import MyTodo from './components/MyTodo.vue';
+import MyTodo from './components/MyTodo.vue'
 </script>
 
 <template>
   <main>
-    <h1>Todo app</h1>
-    <hr />
+    <v-toolbar title="Todo app" color="light-blue-darken-3"
+      ><v-app-bar-nav-icon></v-app-bar-nav-icon
+    ></v-toolbar>
+
     <MyTodo />
   </main>
 </template>
--- src/components/MyTodo.vue
+++ src/components/MyTodo.vue
@@ -1,56 +1,91 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-const newItem = ref("");
+const newItem = ref('')
 
-const todoList = ref([
+//Mockup todo list
+/* const todoList = ref([
     { id: 1, text: "Steuererklärung machen" },
     { id: 2, text: "Müll runterbringen" },
     { id: 3, text: "Abwasch machen" },
     { id: 4, text: "Vue Tutorials abolvieren" },
     { id: 5, text: "Meditieren" }
-]);
+]); */
+
+//Empty List
+const todoList = ref([])
 
 function handleSubmit() {
-    todoList.value.push({ id: todoList.value.length + 1, text: newItem.value });
-    newItem.value = "";
+  todoList.value.push({ id: todoList.value.length + 1, text: newItem.value })
+  newItem.value = ''
 }
-
-
 </script>
 
 <template>
-    <v-container class="mx-auto w-75">
-        <v-toolbar :elevation="1" title="This is a Todo List"></v-toolbar>
-        <br>
-        <v-form @submit.prevent="handleSubmit" class="w-50 my-10" max-width="300px">
-            <v-text-field v-model.trim="newItem" placeholder="Add an item..."></v-text-field>
-            <v-btn type="submit">Save Item <v-icon icon="mdi-plus-circle"></v-icon></v-btn>
-        </v-form>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card color="light-blue-darken-3">
+          <v-row>
+            <v-col cols="12"><v-card-title>My Todo List App</v-card-title></v-col>
 
-        <v-card variant="tonal" max-width="300px">
-            <v-card-text>{{ newItem }}</v-card-text>
+            <v-card-text>
+              <!-- Formular -->
+              <v-col cols="12" justify="center">
+                <v-form @submit.prevent="handleSubmit">
+                  <v-text-field
+                    v-model.trim="newItem"
+                    placeholder="Add something to do..."
+                  ></v-text-field>
+                  <v-btn type="submit" color="light-blue-darken-4"
+                    >Save Item <v-icon icon="mdi-plus-circle"></v-icon
+                  ></v-btn>
+                </v-form>
+              </v-col>
+              <!--
+            <v-card variant="tonal" max-width="300px">
+                <v-card-text>{{ newItem }}</v-card-text>
+            </v-card> 
+            -->
+
+              <!-- Einfache Liste -->
+              <!--     
+            <v-list lines="one">
+                <v-list-item v-for="entry in todoList" :key="entry" :title="entry.text"></v-list-item>
+            </v-list> 
+            -->
+
+              <!-- Card Liste mit Hover-Effekt -->
+              <v-col cols="12" align-self="center">
+                <v-card color="light-blue-darken-1" v-if="todoList.length !== 0">
+                  <v-list
+                    :items="todoList"
+                    item-title="text"
+                    item-value="text"
+                    class="text-center"
+                    bg-color="light-blue-darken-2"
+                  ></v-list>
+                </v-card>
+              </v-col>
+            </v-card-text>
+          </v-row>
         </v-card>
-
-        <!-- Einfache Liste -->
-        <!--     <v-list lines="one">
-        <v-list-item v-for="entry in todoList" :key="entry" :title="entry.text"></v-list-item>
-    </v-list> -->
-
-        <!-- Card Liste mit Hover-Effekt -->
-        <v-card max-width="300">
-            <v-list :items="todoList" item-title="text" item-value="text"></v-list>
-        </v-card>
-    </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style>
+body {
+  background-color: #01579b;
+}
+h1 {
+  text-align: center;
+}
 .todoList {
-    list-style-type: square;
-    color: aquamarine;
-
-    :hover {
-        color: aqua;
-    }
+  color: aquamarine;
+  :hover {
+    color: aqua;
+  }
 }
 </style>
